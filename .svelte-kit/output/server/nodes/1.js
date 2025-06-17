3507bc83303168e57dde97ffebf1c84e804f4030
wefantasy <wefantasy@users.noreply.github.com>
2025-06-17 09:58:02 +0000

    fix: icon dislocation

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BEgUKIK7.js","_app/immutable/chunks/BAuLwB1o.js","_app/immutable/chunks/BCYs_ujV.js","_app/immutable/chunks/DF3BP7tu.js","_app/immutable/chunks/Caowywx8.js"];
-export const stylesheets = [];
-export const fonts = [];
